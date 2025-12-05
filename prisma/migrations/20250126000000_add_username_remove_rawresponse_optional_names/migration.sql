a9318f7d1dfad9b602cd266a60ffa308837a8ae9
HenshawIII <Henshawimmanuel@gmail.com>
2025-12-05 22:10:04 +0100

    migrations manually updated

---
+++ prisma/migrations/20250126000000_add_username_remove_rawresponse_optional_names/migration.sql
@@ -0,0 +1,21 @@
+-- AlterTable: Add username field to User table
+ALTER TABLE "User" ADD COLUMN "username" TEXT;
+
+-- CreateIndex: Create unique index on username
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- AlterTable: Make firstName optional in User table
+ALTER TABLE "User" ALTER COLUMN "firstName" DROP NOT NULL;
+
+-- AlterTable: Make lastName optional in User table
+ALTER TABLE "User" ALTER COLUMN "lastName" DROP NOT NULL;
+
+-- AlterTable: Remove rawResponse from NinVerification table
+ALTER TABLE "NinVerification" DROP COLUMN IF EXISTS "rawResponse";
+
+-- AlterTable: Remove rawResponse from BvnVerification table
+ALTER TABLE "BvnVerification" DROP COLUMN IF EXISTS "rawResponse";
+
+-- AlterTable: Remove rawResponse from AddressVerification table
+ALTER TABLE "AddressVerification" DROP COLUMN IF EXISTS "rawResponse";
+
